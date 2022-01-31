7210bccbebbe2c48e1571c87537e8ba054c56389
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2022-01-31 14:20:23 -0600

    Curso Ionic | Guardando Articulo localmente

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -3,6 +3,7 @@ import { Article } from '../../interfaces/index';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 import { ActionSheetButton, ActionSheetController, Platform } from '@ionic/angular';
 import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
+import { StorageService } from '../../services/storage.service';
 
 @Component({
   selector: 'app-article',
@@ -18,7 +19,8 @@ export class ArticleComponent {
     private iab : InAppBrowser, 
     private platform : Platform, 
     private actionSheetCtrl : ActionSheetController, 
-    private socialSharing : SocialSharing) { }
+    private socialSharing : SocialSharing,
+    private storageService : StorageService) { }
 
 
   openArticle(){
@@ -96,7 +98,8 @@ export class ArticleComponent {
   }
 
   onToggleFavorite(){
-    console.log('Toggle favorite')
+    //console.log('Toggle favorite')
+    this.storageService.saveRemoveArticle(this.article)
   }
 
 }
